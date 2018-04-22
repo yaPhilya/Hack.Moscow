a6fcc54dcd96c5c021266f8cb94e0b17e37b8e3a
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2018-04-22 11:07:26 +0300

    move deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,3 @@ set -e
 
 docker build -t strangeducttape/bluecat_backend .
 docker push strangeducttape/bluecat_backend
-
-scp ./docker-compose.yaml root@188.246.233.30:/app/backend
-
-ssh root@188.246.233.30 "cd /app/backend && docker stack deploy --compose-file docker-compose.yaml backend"
